3269147048cc5c3f961fa29814b750e9fca0e911
dimko <dmitri.lisen@mail.ru>
2020-02-18 13:07:55 +0300

    binding adapter for recyclerView changed animations while list loading added

--- app/src/main/java/post/BindingUtils.kt
+++ app/src/main/java/post/BindingUtils.kt
@@ -7,17 +7,20 @@ import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.simplelifehackerapp.R
 import database.pojo.Article
 import kotlinx.android.synthetic.main.list_item_post.view.*
 
 @BindingAdapter("postImage")
-fun ImageView.setPostImage(article: Article) {
-    Glide.with(this.context)
+fun setPostImage(imageView: ImageView, article: Article) {
+    Glide.with(imageView.context)
         .load(article.catCover.sizes.mobile)
-        .placeholder(R.drawable.ic_broken_image_black_24dp)
+        .apply(RequestOptions()
+            .placeholder(R.drawable.loading_animation)
+            .error(R.drawable.ic_broken_image_black_24dp))
         .centerCrop()
-        .into(this)
+        .into(imageView)
 }
 
 @BindingAdapter("postArticle")
